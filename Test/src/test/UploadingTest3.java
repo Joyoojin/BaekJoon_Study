4db65d8fdfbafcbb23b4827250c87ef6d32fa5a0
Joyoojin <90246581+Joyoojin@users.noreply.github.com>
2021-10-23 14:34:03 +0900

    Add files via upload

---
+++ Test/src/test/UploadingTest3.java
@@ -0,0 +1,10 @@
+package uploadingTest;
+
+public class UploadingTest3 {
+	public static void main(String[] args) {
+		System.out.println("Uploading Test");
+		System.out.println(3);
+		System.out.println("eclipse_workspace_java__TeamStudy_uploadingtest_uploading_from eclipse to githup website by dragging ");	
+	}
+	
+}
