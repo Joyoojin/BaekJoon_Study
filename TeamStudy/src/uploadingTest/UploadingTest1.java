f85d13238b42d7709b841c4c1715cf7c283d7178
Joyoojin <yooxx1001@naver.com>
2021-10-23 13:56:58 +0900

    test

---
+++ TeamStudy/src/uploadingTest/UploadingTest1.java
@@ -0,0 +1,9 @@
+package uploadingTest;
+
+public class UploadingTest1 {
+	public static void main(String[] args) {
+		System.out.println("Uploading Test");
+		System.out.println(1);
+		System.out.println("eclipse_workspace_java__TeamStudy_uploadingtest_uploading");
+	}
+}
